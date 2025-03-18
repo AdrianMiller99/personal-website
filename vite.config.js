08f5af4184426815b42ffd8577db4c6f18aefbc2
Adrian Miller <adrian.miller@students.fhnw.ch>
2025-03-18 05:28:36 +0100

    fixed unable to refresh page bug

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Uncomment this line when deploying to GitHub Pages if not using a custom domain
   // base: '/personal-website/',
+  // When using HashRouter and a custom domain, we can leave base as '/'
+  base: '/',
   optimizeDeps: {
     include: ['react-pdf'],
   },
